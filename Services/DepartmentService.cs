f7b23c8f86f225f029f17995f53548f3f8945d7f
MinaAl5al <minanasser82018@gmail.com>
2025-04-02 00:05:24 +0200

    IDepartmentService.cs DepartmentService.cs

---
+++ Services/DepartmentService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Models;
+using WebAPI.Repository;
+
+namespace Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            return _departmentRepository.GetAll();
+        }
+
+        public Department GetDepartmentById(int id)
+        {
+            return _departmentRepository.GetById(id);
+        }
+
+        public void AddDepartment(Department department)
+        {
+            _departmentRepository.Add(department);
+            _departmentRepository.Save();
+        }
+
+        public void UpdateDepartment(Department department)
+        {
+            _departmentRepository.Update(department);
+            _departmentRepository.Save();
+        }
+
+        public void DeleteDepartment(int id)
+        {
+            _departmentRepository.Delete(id);
+            _departmentRepository.Save();
+        }
+    }
+
+}
---
+++ Services/IDepartmentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace Services
+{
+    public interface IDepartmentService
+    {
+        List<Department> GetAllDepartments();
+        Department GetDepartmentById(int id);
+        void AddDepartment(Department department);
+        void UpdateDepartment(Department department);
+        void DeleteDepartment(int id);
+    }
+}
