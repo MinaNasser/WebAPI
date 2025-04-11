86df8f142bacbbb8cbe363866707276d21fdfc81
MinaAl5al <minanasser82018@gmail.com>
2025-04-11 14:45:40 +0200

    Department DTO

---
+++ ViewModels/DepartmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.ViewModels;
+
+namespace ViewModels
+{
+    public class DepartmentDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? ManagerName { get; set; }
+        public List<EmpWithDeptListViewModel>? Employees { get; set; }
+    }
+}
