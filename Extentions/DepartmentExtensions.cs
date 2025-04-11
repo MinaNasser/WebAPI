4a04149f2bc4bef246585629b98cfc51b202d09f
MinaAl5al <minanasser82018@gmail.com>
2025-04-11 14:44:36 +0200

    DepartmentExtensions

---
+++ Extentions/DepartmentExtensions.cs
@@ -0,0 +1,33 @@
+﻿using ViewModels;
+using WebAPI.Extentions;
+using WebAPI.Models;
+
+
+namespace WebAPI.Extensions
+{
+    public static class DepartmentExtensions
+    {
+        public static DepartmentDTO ToDTO(this Department department)
+        {
+            return new DepartmentDTO
+            {
+                Id = department.Id,
+                Name = department.Name,
+                ManagerName = department.ManagerName,
+                Employees = department.Employees?.Select(e => e.ToDTO()).ToList()
+            };
+        }
+
+        public static Department ToModel(this DepartmentDTO dto)
+        {
+            return new Department
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                ManagerName = dto.ManagerName,
+                Employees = dto.Employees?.Select(e => e.ToModel()).ToList()
+            };
+        }
+
+    }
+}
